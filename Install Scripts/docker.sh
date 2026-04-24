9cf40add806820080e94ce74df3484b731de9bdf
plorazxi <paulo.ss.loraschi@gmail.com>
2026-04-24 12:29:43 -0300

    Adicionando reinicialização automática

--- Install Scripts/docker.sh
+++ Install Scripts/docker.sh
@@ -4,6 +4,23 @@ sudo pacman -Syu docker
 # Adicionando o USER ao grupo do Docker
 sudo usermod -aG docker $USER
 
-# Aplicando permissões 
-echo -e "\e[31mAplicando permissões, porém o recomendado é reiniciar o PC\e[0m"
-newgrp docker
+# Perguntando se reiniciar agora
+read -p "Reiniciar agora? [Y/n] " resposta
+
+case "$resposta" in
+    [yY][eE][sS]|[yY]|"") 
+        echo "Reiniciando o sistema..."
+        sudo reboot
+        ;;
+    [nN][oO]|[nN])
+        # Aplicando permissões
+        echo -e "\e[31mAplicando permissões, porém o recomendado é reiniciar o PC\e[0m"
+        newgrp docker
+        exit 0
+        ;;
+    *)
+        echo "Opção inválida, saindo..."
+        exit 1
+        ;;
+esac
+
